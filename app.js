9c457055ccf59f52e0cfd838e0ef6aefe0a42cb6
mansao01 <novrilukman15@gmail.com>
2023-12-22 16:21:53 +0700

    add user check is available or not

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const {Client, LocalAuth} = require('whatsapp-web.js');
 const express = require('express');
 const qrcode = require('qrcode-terminal');
 
@@ -7,7 +7,7 @@ let isClientReady = false;
 
 
 const client = new Client({
-    puppeteer: { headless: true },
+    puppeteer: {headless: true},
     authStrategy: new LocalAuth()
 
 });
@@ -16,7 +16,7 @@ client.initialize();
 
 client.on('qr', (qr) => {
     console.log('Scan the QR code with your phone:');
-    qrcode.generate(qr, { small: true });
+    qrcode.generate(qr, {small: true});
 });
 
 client.on('authenticated', (session) => {
@@ -35,30 +35,37 @@ client.on("auth_failure", (msg) => {
     console.log("AUTH FAILURE", msg)
 })
 app.get('/', (req, res) => {
-    res.status(200).json({ msg: 'Welcome to triangle sneacare API' });
+    res.status(200).json({msg: 'Welcome to triangle sneacare API'});
 });
 
-app.post("/send", (req, res) => {
+app.post("/send", async (req, res) => {
     if (!isClientReady) {
         return res.status(400).json({ message: 'Client not ready yet' });
     }
 
-    let destination = req.query.destination;
-    let message = req.query.message;
-
     try {
+        let destination = req.query.destination;
+        let message = req.query.message;
+
+        if (!destination || !message) {
+            return res.status(400).json({ message: 'Destination or message missing' });
+        }
+
         destination = destination.substring(1);
         destination = `62${destination}@c.us`;
+
         console.log(destination);
         console.log(message);
 
-        client.sendMessage(destination, message).then(() => {
-            res.status(200).json({ message: "message sent" });
-        }).catch((error) => {
-            res.status(400).json({ message: error });
-        });
-    } catch (e) {
-        res.status(400).json({ message: e });
+        const checkUser = await client.isRegisteredUser(destination);
+        if (!checkUser) {
+            return res.status(400).json({ message: "User not registered" });
+        }
+
+        await client.sendMessage(destination, message);
+        res.status(200).json({ message: "Message sent successfully" });
+    } catch (error) {
+        res.status(400).json({ message: error.message || 'An error occurred' });
     }
 });
 
