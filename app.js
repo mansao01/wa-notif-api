31e36c5cc3877d9bb59f0b8151d8c56902c9fe1d
mansao01 <novrilukman15@gmail.com>
2023-12-22 14:50:31 +0700

    success, app worked

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-import { Client  } from 'whatsapp-web.js';
-import express from 'express';
-import fs from 'fs';
-import qrcode from 'qrcode-terminal';
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const express = require('express');
+const qrcode = require('qrcode-terminal');
 
 const app = express();
+let isClientReady = false;
 
-const SESSION_FILE_PATH = './session.json';
-let sessionCfg;
-
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
 
 const client = new Client({
     puppeteer: { headless: true },
-    session: sessionCfg
+    authStrategy: new LocalAuth()
 
 });
 
@@ -27,23 +21,45 @@ client.on('qr', (qr) => {
 
 client.on('authenticated', (session) => {
     console.log('Authenticated successfully!', session);
-    sessionCfg = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
+});
+client.on('loading_screen', (percent, message) => {
+    console.log('LOADING SCREEN', percent, message);
 });
 
 client.on('ready', () => {
     console.log('WhatsApp Client is ready!');
+    isClientReady = true; // Update the readiness flag
 });
 
-client.on("auth_failure", (msg) =>{
+client.on("auth_failure", (msg) => {
     console.log("AUTH FAILURE", msg)
 })
 app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Welcome to triangle sneacare API' });
+});
+
+app.post("/send", (req, res) => {
+    if (!isClientReady) {
+        return res.status(400).json({ message: 'Client not ready yet' });
+    }
+
+    let destination = req.query.customerNumber;
+    let message = req.query.message;
+
+    try {
+        destination = destination.substring(1);
+        destination = `62${destination}@c.us`;
+        console.log(destination);
+        console.log(message);
+        
+        client.sendMessage(destination, message).then(() => {
+            res.status(200).json({ message: "success" });
+        }).catch((error) => {
+            res.status(400).json({ message: error });
+        });
+    } catch (e) {
+        res.status(400).json({ message: e });
+    }
 });
 
 const PORT = process.env.PORT || 8000;
